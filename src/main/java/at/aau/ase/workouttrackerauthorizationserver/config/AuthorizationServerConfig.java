e89046d44f74c2f49eb1a1a756264b2f89fefc9a
Timo Tabertshofer <timo.tabertshofer@gmail.com>
2024-01-19 18:59:13 +0100

    Match implementation to official Spring documentation

--- src/main/java/at/aau/ase/workouttrackerauthorizationserver/config/AuthorizationServerConfig.java
+++ src/main/java/at/aau/ase/workouttrackerauthorizationserver/config/AuthorizationServerConfig.java
@@ -17,35 +17,27 @@ package at.aau.ase.workouttrackerauthorizationserver.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
-import java.util.List;
 import java.util.UUID;
-import java.util.function.Consumer;
 
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.OAuth2Error;
-import org.springframework.security.oauth2.core.OAuth2ErrorCodes;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthorizationCodeRequestAuthenticationContext;
-import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthorizationCodeRequestAuthenticationException;
-import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthorizationCodeRequestAuthenticationProvider;
-import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthorizationCodeRequestAuthenticationToken;
-import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthorizationCodeRequestAuthenticationValidator;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -57,105 +49,84 @@ import org.springframework.security.oauth2.server.authorization.settings.TokenSe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 
-import at.aau.ase.workouttrackerauthorizationserver.jose.Jwks;
-
-/**
- * @author Joe Grandja
- * @author Daniel Garnier-Moiroux
- * @author Steve Riesenberg
- * @since 1.1
- */
 @Configuration(proxyBeanMethods = false)
 public class AuthorizationServerConfig {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(AuthorizationServerConfig.class);
+  private static KeyPair generateRsaKey() {
+    KeyPair keyPair;
+    try {
+      KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+      keyPairGenerator.initialize(2048);
+      keyPair = keyPairGenerator.generateKeyPair();
+    } catch (Exception ex) {
+      throw new IllegalStateException(ex);
+    }
+    return keyPair;
+  }
 
   @Bean
-  @Order(Ordered.HIGHEST_PRECEDENCE)
-  public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
-    // https://docs.spring.io/spring-authorization-server/docs/current/reference/html/getting-started.html
-
-    // version 1
-    // Replaced this call with the implementation of applyDefaultSecurity() to be able to add a custom redirect_uri validator
-    // OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-
-    OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
-        new OAuth2AuthorizationServerConfigurer();
-
-    // Register a custom redirect_uri validator, that allows redirect uris based on https://localhost during development
-    authorizationServerConfigurer.authorizationEndpoint(authorizationEndpoint ->
-        authorizationEndpoint.authenticationProviders(configureAuthenticationValidator())
-    );
-
-    RequestMatcher endpointsMatcher = authorizationServerConfigurer.getEndpointsMatcher();
-
-    http
-        .securityMatcher(endpointsMatcher)
-        .authorizeHttpRequests(authorize ->
-            authorize.anyRequest().authenticated()
-        )
-        .csrf(csrf ->
-            csrf.ignoringRequestMatchers(endpointsMatcher)
-        )
-        .apply(authorizationServerConfigurer);
-
-
-    // version 2
-/*     OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-
+  @Order(1)
+  public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
+      throws Exception {
+    OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
     http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-        .authorizationEndpoint(authorizationEndpoint ->
-            authorizationEndpoint.authenticationProviders(configureAuthenticationValidator())
-        ); */
-
-    // from Spring docs
-    http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-        .oidc(withDefaults()); // Enable OpenID Connect 1.0
-
+        .oidc(withDefaults());  // Enable OpenID Connect 1.0
     http
         // Redirect to the login page when not authenticated from the
         // authorization endpoint
-        .exceptionHandling(exceptions ->
-            exceptions.defaultAuthenticationEntryPointFor(
+        .exceptionHandling((exceptions) -> exceptions
+            .defaultAuthenticationEntryPointFor(
                 new LoginUrlAuthenticationEntryPoint("/login"),
                 new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
             )
         )
         // Accept access tokens for User Info and/or Client Registration
-        .oauth2ResourceServer(resourceServer ->
-            resourceServer.jwt(withDefaults())
-        );
+        .oauth2ResourceServer((resourceServer) -> resourceServer
+            .jwt(withDefaults()));
+
+    return http.build();
+  }
+
+  @Bean
+  @Order(2)
+  public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/actuator/**").permitAll()
+            .anyRequest().authenticated()
+        )
+        .formLogin(withDefaults());
 
     return http.build();
   }
 
   @Bean
   public RegisteredClientRepository registeredClientRepository() {
-    // TODO (Timo Tabertshofer, 02.11.2023): Update redirect and logout URIs
     RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
         .clientId("wt-client")
         .clientSecret("{noop}secret")
         .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-        .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
         .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-        .redirectUri("http://my.redirect.uri")
-        .redirectUri("http://localhost:8443/openapi/webjars/swagger-ui/oauth2-redirect.html")
-        .postLogoutRedirectUri("http://my.logout.uri")
-//        .scope(OidcScopes.OPENID) // TODO (Timo Tabertshofer, 02.11.2023): Maybe this is needed but I don't use scopes
+        .redirectUri("")
         .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
         .tokenSettings(TokenSettings.builder().accessTokenTimeToLive(Duration.ofHours(1)).build())
         .build();
 
-    return new InMemoryRegisteredClientRepository(registeredClient);  }
+    return new InMemoryRegisteredClientRepository(registeredClient);
+  }
 
   @Bean
   public JWKSource<SecurityContext> jwkSource() {
-    RSAKey rsaKey = Jwks.generateRsa();
+    KeyPair keyPair = generateRsaKey();
+    RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+    RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+    RSAKey rsaKey = new RSAKey.Builder(publicKey)
+        .privateKey(privateKey)
+        .keyID(UUID.randomUUID().toString())
+        .build();
     JWKSet jwkSet = new JWKSet(rsaKey);
-    return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+    return new ImmutableJWKSet<>(jwkSet);
   }
 
   @Bean
@@ -169,43 +140,6 @@ public class AuthorizationServerConfig {
         // TODO (Timo Tabertshofer, 02.11.2023): Set to http://authorization-server:9999 in docker
         .issuer("http://localhost:9999")
         .build();
-  }
-
-  private Consumer<List<AuthenticationProvider>> configureAuthenticationValidator() {
-    return authenticationProviders ->
-        authenticationProviders.forEach(authenticationProvider -> {
-          if (authenticationProvider instanceof
-              OAuth2AuthorizationCodeRequestAuthenticationProvider oAuth2AuthenticationProvider) {
-            Consumer<OAuth2AuthorizationCodeRequestAuthenticationContext> authenticationValidator =
-                // Override default redirect_uri validator
-                new CustomRedirectUriValidator()
-                    // Reuse default scope validator
-                    .andThen(OAuth2AuthorizationCodeRequestAuthenticationValidator.DEFAULT_SCOPE_VALIDATOR);
-
-            oAuth2AuthenticationProvider.setAuthenticationValidator(authenticationValidator);
-          }
-        });
-  }
-
-  static class CustomRedirectUriValidator implements Consumer<OAuth2AuthorizationCodeRequestAuthenticationContext> {
-
-    @Override
-    public void accept(OAuth2AuthorizationCodeRequestAuthenticationContext authenticationContext) {
-      OAuth2AuthorizationCodeRequestAuthenticationToken authorizationCodeRequestAuthentication =
-          authenticationContext.getAuthentication();
-      RegisteredClient registeredClient = authenticationContext.getRegisteredClient();
-      String requestedRedirectUri = authorizationCodeRequestAuthentication.getRedirectUri();
-
-      LOGGER.info("Will validate the redirect uri {}", requestedRedirectUri);
-
-      // Use exact string matching when comparing client redirect URIs against pre-registered URIs
-      if (!registeredClient.getRedirectUris().contains(requestedRedirectUri)) {
-        LOGGER.info("Redirect uri is invalid!");
-        OAuth2Error error = new OAuth2Error(OAuth2ErrorCodes.INVALID_REQUEST);
-        throw new OAuth2AuthorizationCodeRequestAuthenticationException(error, null);
-      }
-      LOGGER.info("Redirect uri is OK!");
-    }
   }
 
 }
